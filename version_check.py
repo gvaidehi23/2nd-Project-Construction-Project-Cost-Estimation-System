43d03284fa8c334586bea15d3976bfc6ae27fd4d
gvaidehi23 <prachigatkal2523@gmail.com>
2026-06-29 13:36:49 +0530

    Update version_check.py

--- version_check.py
+++ version_check.py
@@ -1,9 +1,9 @@
 import pandas as pd
 import numpy as np
 import sklearn 
-import streamlit 
+import streamlit as st
 
 print(pd.__version__)
 print(np.__version__)
 print(sklearn.__version__)
-print(streamlit.__version__)
+print(st.__version__)
